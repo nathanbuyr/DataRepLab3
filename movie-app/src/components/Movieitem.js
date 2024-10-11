0b92fac442d48dfb08aa084924735f3f72f805d7
nathanbuyr <G00421082@atu.ie>
2024-10-11 14:31:56 +0000

    useEffect added to Movieitem

--- movie-app/src/components/Movieitem.js
+++ movie-app/src/components/Movieitem.js
@@ -1,15 +1,29 @@
-
+import { useEffect } from "react";
+import Card from 'react-bootstrap/Card';
 {/* MovieItem Function*/ }
 {/* Using seprated components,it separates concerns and makes the app modular*/ }
 const MovieItem = (props)=>
 {
+    useEffect(() => {
+        console.log("Movie Item:", props.Mymovies);
+      }, [props.mymovie]); // Only run this effect when the mymovie prop changes
+    
     return(
         <div>
-            <h3>{props.myMovies.Title}</h3>
-            <p>{props.myMovies.Year}</p>
+            <Card>
+            
+            <Card.Header>{props.myMovies.Title}</Card.Header>
+            <Card.Body>
+            <blockquote className="blockquote mb-0">
+            <footer>{props.myMovies.Year}</footer>
             <img src={props.myMovies.Poster}></img>
+            </blockquote>
+            </Card.Body>
+            </Card>
         </div>
     );
 }
+
+
 
 export default MovieItem;
